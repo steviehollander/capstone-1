dadf5734e793851f90a5a11e8b7e0da2fa552a9b
Stevie Hollander <steviehollander@icloud.com>
2022-04-03 14:45:49 -0500

    deployfix

--- src/components/WorldCloset.js
+++ src/components/WorldCloset.js
@@ -68,7 +68,7 @@ export const WorldCloset = () => {
 
     useEffect(
         () => {
-            fetch("hhttps://capstone-1-api-nw5wj.ondigitalocean.app/pieceTypes")
+            fetch("https://capstone-1-api-nw5wj.ondigitalocean.app/pieceTypes")
                 .then(res => res.json())
                 .then((data) => {
                     //getting data back from API and passing to the stter
